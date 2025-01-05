09641ebbdefb02c8a63809f929ccadc6da814e54
Afonso <145401457+afonsohduarte@users.noreply.github.com>
2025-01-05 20:00:13 +0000

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -1,1 +1,13 @@
+package com.santiClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantiClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantiClinicApplication.class, args);
+    }
+}
 
